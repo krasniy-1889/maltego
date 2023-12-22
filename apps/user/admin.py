8c3a98533477fd65d5ca1e17c3f1d72e6ea57c5c
neytirri <129419810+neytirri@users.noreply.github.com>
2023-12-22 07:04:06 +0300

    feat - 'Update nvaigation'

--- apps/user/admin.py
+++ apps/user/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
-from django.contrib.auth.admin import UserAdmin
+from .models import UserProfile
 
 
 @admin.register(get_user_model())
@@ -9,3 +9,8 @@ class CustomUserAdmin(admin.ModelAdmin):
     list_display = ["username", "email", "is_active", "is_staff"]
     search_fields = ["userpermission_classes = [IsAuthenticated]name", "email"]
     ordering = []
+
+
+@admin.register(UserProfile)
+class AdminUserProfile(admin.ModelAdmin):
+    list_display = ["id", "user", "bio"]
--- apps/user/models.py
+++ apps/user/models.py
@@ -1,3 +1,4 @@
+from allauth.conftest import get_user_model
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.translation import gettext_lazy as _
@@ -6,7 +7,6 @@ from .managers import CustomUserManager
 
 
 class CustomUser(AbstractUser):
-    profile = models.OneToOneField("UserProfile", on_delete=models.CASCADE, null=True)
     objects = CustomUserManager()
 
     def __str__(self):
@@ -17,9 +17,9 @@ class CustomUser(AbstractUser):
 
 
 class UserProfile(models.Model):
-    avatar = models.ImageField(upload_to="images/", null=True, blank=True)
+    avatar = models.ImageField(upload_to="avatars/", null=True, blank=True)
     bio = models.TextField(max_length=800, blank=True, null=True)
-    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE)
 
     def __str__(self):
         return self.user.username
