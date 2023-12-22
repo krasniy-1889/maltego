60e689b4f65ce4297aafde7c81554ae7e9cc7f92
neytirri <129419810+neytirri@users.noreply.github.com>
2023-12-22 21:15:43 +0300

    feat: add index
    add neccesery index to Post model

--- apps/post/views.py
+++ apps/post/views.py
@@ -1,9 +1,3 @@
-from typing import Any
-
-from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
-from django.utils.decorators import method_decorator
-from django.views.decorators.cache import cache_page
 from django.views.generic import DetailView, ListView
 
 from .models import Post
@@ -11,17 +5,10 @@ from .models import Post
 
 class PostListViews(ListView):
     model = Post
-    queryset = (
-        Post.objects.select_related(
-            "status",
-            "type",
-        )
-        .prefetch_related(
-            "tags",
-            "genres",
-        )
-        .all()
-    )
+    queryset = Post.objects.prefetch_related(
+        "tags",
+        "genres",
+    ).all()
     template_name = "post/list.html"
     context_object_name = "posts"
     paginate_by = 20
